9ca90b7508698705f5e893af0af77f187cb5ddfe
Diana <dvizh1203@gmail.com>
2025-08-02 12:57:33 +0300

    Update comment.py

--- app/schemas/comment.py
+++ app/schemas/comment.py
@@ -1,1 +1,20 @@
+from pydantic import BaseModel
+from datetime import datetime
+from app.schemas.user import User
 
+class CommentBase(BaseModel):
+    content: str
+
+class CommentCreate(CommentBase):
+    pass
+
+class CommentUpdate(BaseModel):
+    content: Optional[str] = None
+
+class Comment(CommentBase):
+    id: int
+    author: User
+    created_at: datetime
+    
+    class Config:
+        orm_mode = True
