80049fe3e590ef60dee048a52345bae75d9b7cd5
Diana <dvizh1203@gmail.com>
2025-08-02 12:57:48 +0300

    Update tag.py

--- app/schemas/tag.py
+++ app/schemas/tag.py
@@ -1,1 +1,18 @@
+from pydantic import BaseModel
+from app.schemas.post import Post
 
+class TagBase(BaseModel):
+    name: str
+
+class TagCreate(TagBase):
+    pass
+
+class TagUpdate(BaseModel):
+    name: Optional[str] = None
+
+class Tag(TagBase):
+    id: int
+    posts: list[Post] = []
+    
+    class Config:
+        orm_mode = True
