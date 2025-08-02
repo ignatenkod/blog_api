24c29974e41418a2ef46b1063b45fe5c6f689acf
Diana <dvizh1203@gmail.com>
2025-08-02 12:53:57 +0300

    Update cache.py

--- app/core/cache.py
+++ app/core/cache.py
@@ -1,1 +1,16 @@
+from redis.asyncio import Redis
+from app.core.config import settings
 
+redis: Optional[Redis] = None
+
+async def get_redis() -> Redis:
+    return redis
+
+async def init_redis() -> None:
+    global redis
+    redis = Redis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        password=settings.REDIS_PASSWORD,
+        decode_responses=True
+    )
