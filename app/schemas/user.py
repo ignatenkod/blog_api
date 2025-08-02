d1ef1fcc202b343a05342dcc0f9c9022e1e2ed1d
Diana <dvizh1203@gmail.com>
2025-08-02 12:57:00 +0300

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,1 +1,29 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
 
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: Optional[str] = None
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = None
+    full_name: Optional[str] = None
+    password: Optional[str] = None
+
+class UserInDBBase(UserBase):
+    id: int
+    is_active: bool
+    is_superuser: bool
+    
+    class Config:
+        orm_mode = True
+
+class User(UserInDBBase):
+    pass
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
