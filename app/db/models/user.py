24853c684ec06352ab3456ba745530f0e8638c7e
Diana <dvizh1203@gmail.com>
2025-08-02 12:55:01 +0300

    Update user.py

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.db.base_class import Base
 
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    full_name = Column(String)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
+    
+    posts = relationship("Post", back_populates="author")
+    comments = relationship("Comment", back_populates="author")
