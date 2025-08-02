bab180865d979f7ba933b9aac412e445a62d85b2
Diana <dvizh1203@gmail.com>
2025-08-02 13:01:24 +0300

    Update users.py

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,1 +1,70 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
+from app.crud.user import user
+from app.db.session import get_db
+from app.schemas.user import User, UserCreate, UserUpdate
+from app.core.security import get_current_active_superuser, get_current_active_user
 
+router = APIRouter()
+
+@router.get("/", response_model=List[User])
+async def read_users(
+    db: AsyncSession = Depends(get_db),
+    skip: int = 0,
+    limit: int = 100,
+    current_user: User = Depends(get_current_active_superuser),
+):
+    users = await user.get_multi(db, skip=skip, limit=limit)
+    return users
+
+@router.post("/", response_model=User)
+async def create_user(
+    *,
+    db: AsyncSession = Depends(get_db),
+    user_in: UserCreate,
+    current_user: User = Depends(get_current_active_superuser),
+):
+    db_user = await user.get_by_email(db, email=user_in.email)
+    if db_user:
+        raise HTTPException(
+            status_code=400,
+            detail="The user with this email already exists.",
+        )
+    return await user.create(db, obj_in=user_in)
+
+@router.get("/me", response_model=User)
+async def read_user_me(
+    current_user: User = Depends(get_current_active_user),
+):
+    return current_user
+
+@router.get("/{user_id}", response_model=User)
+async def read_user(
+    user_id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_active_superuser),
+):
+    db_user = await user.get(db, id=user_id)
+    if not db_user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this id does not exist.",
+        )
+    return db_user
+
+@router.put("/{user_id}", response_model=User)
+async def update_user(
+    *,
+    db: AsyncSession = Depends(get_db),
+    user_id: int,
+    user_in: UserUpdate,
+    current_user: User = Depends(get_current_active_superuser),
+):
+    db_user = await user.get(db, id=user_id)
+    if not db_user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this id does not exist.",
+        )
+    return await user.update(db, db_obj=db_user, obj_in=user_in)
