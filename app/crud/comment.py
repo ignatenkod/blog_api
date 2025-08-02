645c7fb224570b2a3fa6e07ee0186a105f9acb48
Diana <dvizh1203@gmail.com>
2025-08-02 12:59:16 +0300

    Update comment.py

--- app/crud/comment.py
+++ app/crud/comment.py
@@ -1,1 +1,20 @@
+from typing import List
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.crud.base import CRUDBase
+from app.db.models.comment import Comment
+from app.schemas.comment import CommentCreate, CommentUpdate
 
+class CRUDComment(CRUDBase[Comment, CommentCreate, CommentUpdate]):
+    async def get_multi_by_post(
+        self, db: AsyncSession, *, post_id: int, skip: int = 0, limit: int = 100
+    ) -> List[Comment]:
+        result = await db.execute(
+            select(self.model)
+            .filter(self.model.post_id == post_id)
+            .offset(skip)
+            .limit(limit)
+        )
+        return result.scalars().all()
+
+comment = CRUDComment(Comment)
